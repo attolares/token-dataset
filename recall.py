3d1f4dfac878199dc6bc1f31a502723380df1e95
Lari <larissaesilva@gmail.com>
2020-09-02 14:24:21 -0300

    fix'd

--- recall.py
+++ recall.py
@@ -1,5 +1,5 @@
-
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_fscore_support
 import matplotlib.pyplot as plt  
 import pandas
 import numpy as np
@@ -17,9 +17,20 @@ cm = confusion_matrix(true_values, predictions, labels)
 
 print(cm)
 
+cn = cm / cm.astype(np.float).sum(axis=1)
 
+#cm = np.true_divide(cm,cm.astype(np.float).sum(axis=1))
+
+norm = np.zeros((len(labels),len(labels)))
+
+for i in range(len(labels)):
+    for j in range(len(labels)):
+        if (cn[i,j] > 0):
+            norm[i,j] = cn[i,j]
+
+cn = norm
 fig, ax = plt.subplots()
-im = ax.imshow(cm)
+im = ax.imshow(cn)
 
 
 ax.set_xticks(np.arange(len(labels)))
@@ -34,8 +45,9 @@ plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
 # por o valor nas celulas
 for i in range(len(labels)):
     for j in range(len(labels)):
-        text = ax.text(j, i, cm[i, j],
-                       ha="center", va="center", color="w", size="smaller")
+        if(cn[i,j]>0 and cn[i,j]<= 1):
+            text = ax.text(j, i, round(cn[i,j],2),
+                           ha="center", va="center", color="b", size=6)
 
 ax.set_title("Matriz")
 fig.tight_layout()
